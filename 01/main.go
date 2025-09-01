56f2c365380c1d9f914c63e572519eea445c00f4
dslcosta1 <dslcosta2016@gmail.com>
2025-09-01 08:51:48 -0300

    Add http tests

---
+++ 01/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+)
+
+type api struct {
+	addr string
+}
+
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Users list..."))
+}
+
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Created users..."))
+}
+
+func main() {
+	api := &api{addr: ":8080"}
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr: api.addr,
+		Handler: mux,
+	}
+
+	mux.HandleFunc("GET /users", api.getUsersHandler)
+	mux.HandleFunc("POST /users", api.createUsersHandler)
+	
+	srv.ListenAndServe()
+
+}
